4992785dea682335070d745c5ab60cea3fa04fd7
Michele773 <151859741+Michele773@users.noreply.github.com>
2024-01-05 13:27:05 +0100

    Update streamlit_ufq_app.py
    first text

--- streamlit_ufq_app.py
+++ streamlit_ufq_app.py
@@ -1,6 +1,16 @@
 # UFQ_APP Create a Prototype with Snowflake and Phyton of ufq 
 
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('Hallo Herwig das hier ist die Demo Page')
+streamlit.header('This is how the World is swinging')
 
+streamlit.header('Frequences')
+streamlit.text('🥣 Planeten')
+streamlit.text('🥗 Instrumente')
+streamlit.text('🐔 Water')
+streamlit.text('🥑🍞 Earth')
