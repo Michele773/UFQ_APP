a51cca231dba78770dfdcc5cea0dbefe90efe690
Michele773 <151859741+Michele773@users.noreply.github.com>
2024-01-05 13:55:57 +0100

    Update streamlit_ufq_app.py

--- streamlit_ufq_app.py
+++ streamlit_ufq_app.py
@@ -2,6 +2,7 @@
 
 import streamlit
 import pandas
+import requests
 
 streamlit.title('Hallo Herwig here is the Demo Page')
 streamlit.header('This is how the World is swinging')
@@ -27,3 +28,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
+
+#create the repeatable code blocke (function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+# New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+       streamlit.error("Please select a stupid fruit you dummy.")
+  else:
+       back_from_function = get_fruityvice_data(fruit_choice)
+       streamlit.dataframe(back_from_function)
+	   
+except URLerror as e:
+      streamlit.error()
+
