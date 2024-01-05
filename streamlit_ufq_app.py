b875c7efb45d438297be33158f6d776708da3f8c
Michele773 <151859741+Michele773@users.noreply.github.com>
2024-01-05 13:37:21 +0100

    Update streamlit_ufq_app.py
    imp panda

--- streamlit_ufq_app.py
+++ streamlit_ufq_app.py
@@ -11,3 +11,15 @@ streamlit.text('🥣 Planeten')
 streamlit.text('🥗 Instrumente')
 streamlit.text('🥑 Water')
 streamlit.text('🍞 Earth')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# Display the table on the page.
+
+#import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Frequences are')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some Frequences:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
