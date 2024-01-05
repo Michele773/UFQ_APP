d8c287026b33648b5322ba828b1817696d1255c6
Michele773 <151859741+Michele773@users.noreply.github.com>
2024-01-05 13:13:18 +0100

    Update streamlit_app.py
    intro

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
-# UFQ_APP
-Create a Prototype with Snowflake and Phyton of ufq 
+# UFQ_APP Create a Prototype with Snowflake and Phyton of ufq 
+
+import streamlit
+
+streamlit.title('Hallo Herwig das hier ist die Demo Page')
+
