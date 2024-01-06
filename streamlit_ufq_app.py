6a19e15eb7f2e0f93a26ea21400692c171d20c07
Michele773 <151859741+Michele773@users.noreply.github.com>
2024-01-06 19:36:26 +0100

    Update streamlit_ufq_app.py
    ufq

--- streamlit_ufq_app.py
+++ streamlit_ufq_app.py
@@ -60,7 +60,7 @@ def get_fruit_load_list():
        return my_cur.fetchall()
 
 #Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Here you get the Planets from Snowflake Database'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
