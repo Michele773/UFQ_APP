c94a90558a9ddff16cff6e2f5b4d3b8f2869db63
Michele773 <151859741+Michele773@users.noreply.github.com>
2024-01-06 19:53:28 +0100

    Update streamlit_ufq_app.py
    doc

--- streamlit_ufq_app.py
+++ streamlit_ufq_app.py
@@ -52,7 +52,7 @@ except URLerror as e:
 
 #import snowflake.connector
 
-streamlit.header("View Our Fruit List - Add Your Favorites!")
+streamlit.header("View you get the Planets - Add Your Favorites!")
 #Snowflake-related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
